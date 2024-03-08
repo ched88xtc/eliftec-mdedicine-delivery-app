48d075d32e481c1ab9e5f7bfc569e14069fab862
ched88xtc <ched88official@gmail.com>
2024-03-08 15:17:44 +0200

    setup index.js for deploy

--- Backend/index.js
+++ Backend/index.js
@@ -1,46 +1,54 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import { orderValidation } from './validations/order.js';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import { orderValidation } from "./validations/order.js";
+import cors from "cors";
 
-import {StoresController, OrderController, MedicationController, CouponController} from './controllers/index.js'
+import {
+  StoresController,
+  OrderController,
+  MedicationController,
+  CouponController,
+} from "./controllers/index.js";
 
 const PORT = 4444;
 
 mongoose
-  .connect('mongodb+srv://meddelivery:meddelivery@clustermeddelivery.j5c3sej.mongodb.net/medDelivery?retryWrites=true&w=majority&appName=ClusterMedDelivery')
-  .then(() => console.log('>>> DataBase OK'))
-  .catch((err) => console.log('>>> DataBase error!!!', err));
+  .connect(
+    process.env.MONGO_DB_URI ||
+      "mongodb+srv://meddelivery:meddelivery@clustermeddelivery.j5c3sej.mongodb.net/medDelivery?retryWrites=true&w=majority&appName=ClusterMedDelivery"
+  )
+  .then(() => console.log(">>> DataBase OK"))
+  .catch((err) => console.log(">>> DataBase error!!!", err));
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.listen(PORT, (err) => {
+app.listen(process.env.PORT || PORT, (err) => {
   if (err) {
     return console.log(err);
   }
 
   console.log(`>>> Server is running on port ${PORT}`);
-})
+});
 
 // Routes
 
 // ORDER
-app.post('/orders', orderValidation, OrderController.create);
-app.get('/orders/:orderId', OrderController.getByOrderID);
-app.get('/orders/phone/:phone', OrderController.getByPhone);
-app.get('/orders/email/:email', OrderController.getByEmail);
+app.post("/orders", orderValidation, OrderController.create);
+app.get("/orders/:orderId", OrderController.getByOrderID);
+app.get("/orders/phone/:phone", OrderController.getByPhone);
+app.get("/orders/email/:email", OrderController.getByEmail);
 
 // DRUG STORES
-app.get('/drugstores', StoresController.getAll);
-app.get('/drugstores/:id', StoresController.getById);
+app.get("/drugstores", StoresController.getAll);
+app.get("/drugstores/:id", StoresController.getById);
 
 // MEDICATIONS
-app.get('/medications/:storeId', MedicationController.getByStoreId);
-app.patch('/medications/:medId', MedicationController.updateIsFavorite);
+app.get("/medications/:storeId", MedicationController.getByStoreId);
+app.patch("/medications/:medId", MedicationController.updateIsFavorite);
 
 // COUPONS
-app.get('/coupons', CouponController.getAll);
-app.get('/coupons/:couponCode', CouponController.getByCode);
+app.get("/coupons", CouponController.getAll);
+app.get("/coupons/:couponCode", CouponController.getByCode);
